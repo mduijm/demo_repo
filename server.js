ddfe7bbe278f06e0f69d2fcffcbd2a073ecc18ba
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-10-03 14:47:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ http.createServer(function(req,res){
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write(req.url);
       res.end();		
-      }).listen(3333);
+      }).listen(8081);
 console.log('Server started on localhost:3333; press Ctrl-C to terminate...!');
