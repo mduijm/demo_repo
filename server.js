6ee6dc75c7708cb30789c61c3aac9db39269066b
xxradar <xxradar@radarhack.com>
2023-10-03 12:22:19 +0000

    Pushing my first code to git

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function(req,res){
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write(req.url);
+      res.end();		
+      }).listen(3333);
+console.log('Server started on localhost:3333; press Ctrl-C to terminate...!');
